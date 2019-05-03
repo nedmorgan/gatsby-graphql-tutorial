ecabe6f1aeb85b5e03539f05c033fe536123aa30
nedmorgan <nmorgan1987@gmail.com>
2019-05-03 15:38:33 -0400

    added gatsby file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: `Pandas Like to Nom Nom Nom`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
