dff93edd764e573c8fbfc10fd03336c95cb1dc8c
nedmorgan <nmorgan1987@gmail.com>
2019-05-03 12:22:33 -0400

    created a simple panda blog site

---
+++ src/pages/about.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>About Pandas Eating Tons</h1>
+    <p>
+      paleo McSweeney's swag retro vinyl ethical Etsy polaroid Helvetica twee
+      scenester Tonx meh normcore Thundercats single-origin coffee shabby chic
+      master cleanse Tumblr Cosby sweater pug yr bicycle rights try-hard lomo
+      Kickstarter slow-carb literally whatever chia American Apparel sartorial
+      skateboard tattooed Austin narwhal fanny pack Godard bitters iPhone
+      Bushwick locavore before they sold out typewriter cray mixtape mlkshk
+      ennui High Life disrupt salvia aesthetic distillery umami crucifix occupy
+      sustainable actually PBR&B mumblecore wayfarers 8-bit DIY Odd Future +1
+      semiotics authentic banh mi Echo Park kogi direct trade small batch
+      bespoke gastropub selfies cliche keffiyeh photo booth put a bird on it
+      squid 3 wolf moon drinking vinegar quinoa post-ironic banjo fingerstache
+      four loko messenger bag freegan hashtag stumptown organic 90's YOLO
+      meggings keytar chambray wolf food truck street art blog Intelligentsia
+      flannel leggings jean shorts Truffaut cornhole Marfa selvage forage next
+      level kale chips dreamcatcher letterpress hoodie VHS pickled Pitchfork
+      cardigan fixie lo-fi asymmetrical chillwave Shoreditch fap vegan Carles
+      pop-up XOXO plaid Pinterest farm-to-table seitan trust fund gluten-free
+      raw denim butcher Wes Anderson roof party ugh biodiesel brunch artisan
+      deep v pork belly mustache Schlitz Vice craft beer Brooklyn church-key
+      irony Neutra tote bag tousled kitsch pour-over fashion axe art party beard
+      sriracha cred tofu Banksy hella Portland viral Blue Bottle PBR readymade
+      Williamsburg synth gentrify you probably haven't heard of them heirloom
+      flexitarian
+    </p>
+  </Layout>
+)
