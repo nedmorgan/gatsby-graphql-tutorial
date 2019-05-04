a6230f714388a0142753067260cdd3567587db03
nedmorgan <nmorgan1987@gmail.com>
2019-05-04 12:32:24 -0400

    completed tutorial and deployed via surge

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,26 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
+    "gatsby-plugin-offline",
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
       },
     },
   ],
